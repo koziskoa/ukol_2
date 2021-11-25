b3537814b3b4820f07c6dea545982fed41351554
Anna Kožíšková <koziskoa@natur.cuni.cz>
2021-11-25 17:08:41 +0100

    umí počítat sedmidenní průtoky, ale ne zbytek

--- average_flows.py
+++ average_flows.py
@@ -1,9 +1,46 @@
 import csv
-from os import read
+import math
 with open("priklad_vstupu.csv", encoding="utf-8") as csvfile:
-    reader = csv.reader(csvfile, delimiter = ",")
+     #open ("vystup.csv", encoding="utf-8") as csv_outfile:
+    reader = csv.reader(csvfile, delimiter =",") 
+    #writer = csv.writer(csv_outfile, delimiter = ",")
     
-    for row in reader:  # vytiskne soubor .csv
-        print(row[:])
+    #for row in reader:  # vytiskne soubor .csv
+    #    print(row)
 #když připíšu random komentář
-    
+    prumer = 0
+    suma_prutoku = 0
+    celkem = 0
+    lines = 1
+    prutok = 0
+
+    for row in reader:
+        #lines += int(len(list(row))/6)
+        print(lines)
+        try:
+            prutok = float(row[5])
+        except ValueError:
+            prutok = 0
+        if lines %7!=0:  # tady to načítá a tiskne průtoky
+            suma_prutoku += prutok
+        if lines %7==0:
+            print(suma_prutoku)
+            suma_prutoku += prutok
+            prumer = suma_prutoku/7
+            print(f"{prumer:.4f}")
+            suma_prutoku = 0
+            prumer = 0
+        lines += 1
+
+    '''for row in reader:
+        try:
+            prutok = float(row[5])
+            if pocet_dni % 7 !=0:
+                celkem =+ prutok
+            if pocet_dni == 7:
+                suma_prutoku == 0
+                pocet_dni == 0
+        except ValueError:
+            prutok = 0
+        finally:
+            pocet_dni =+ 1'''
