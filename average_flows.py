dc889156f7a105d5720a1e10bea83c77151c170f
Anna Kožíšková <koziskoa@natur.cuni.cz>
2021-11-27 17:28:25 +0100

    program hotový bez rozdělení do funkcí a komentářů

--- average_flows.py
+++ average_flows.py
@@ -1,8 +1,8 @@
 import csv
-with open("priklad_vstupu.csv", encoding="utf-8") as csvfile,\
-     open ("vystup_7dni.csv", "w", newline="", encoding="utf-8") as csv_outfile:
+with open("QD_200550_Data.csv", encoding="utf-8") as csvfile,\
+     open ("vystup_7dni.csv", "w", newline="", encoding="utf-8") as csv_outfile_tyden:
     reader = csv.reader(csvfile, delimiter =",") 
-    writer = csv.writer(csv_outfile)
+    writer = csv.writer(csv_outfile_tyden)
     
     prumer = 0
     suma_prutoku = 0
@@ -19,10 +19,9 @@ with open("priklad_vstupu.csv", encoding="utf-8") as csvfile,\
             prutok = float(row[5])
             suma_prutoku += prutok
         except ValueError:
-            chyba += 1  # když nastane špatný vtup započítá ho do proměnné chyba
-                  
+            chyba += 1  # když nastane špatný vtup započítá ho do proměnné chyba a vytiskne do konzole informaci o chybě
+            print(f"Pro den {row[4]}. {row[3]}. {row[2]} není možné započítat průtok")
         if lines %7==0:  # když přijde sedmý den (řádek), sečtené průtoky se zprůměrují 
-            #print(suma_prutoku)
             if lines != chyba:
                 prumer = suma_prutoku/(7-chyba)
                 tydenni[5] = f"    {prumer:.4f}"
@@ -47,11 +46,12 @@ with open("priklad_vstupu.csv", encoding="utf-8") as csvfile,\
         print(f"{prumer:.4f}")
         writer.writerow(tydenni)
 
-with open("vstup_6dni.csv", encoding="utf-8") as csvfile,\
+with open("QD_200550_Data.csv", encoding="utf-8") as csvfile,\
      open ("vystup_rok.csv", "w", newline="", encoding="utf-8") as csv_outRok:
     reader = csv.reader(csvfile, delimiter =",") 
     writer = csv.writer(csv_outRok)     
 
+    chyba = 0
     suma_prutok_rok = 0
     lines = 1
     rocni = []
@@ -62,28 +62,46 @@ with open("vstup_6dni.csv", encoding="utf-8") as csvfile,\
             prutok = float(row[5])
             suma_prutok_rok = suma_prutok_rok + prutok
         except ValueError:
-            pass
-        if rok == int(row[2]):
+            if rok != int(row[2]):
+                chyba = 1
+                if rok != 0:
+                    #sem narvat vše pro nový rok
+                    prumer = suma_prutok_rok/(lines-chyba)
+                    print(prumer)
+                    prvni_den[5] = f"   {prumer:.4f}" 
+                    writer.writerow(prvni_den)
+                    rok = int(row[2])   # nese informaci o aktuálním roku v řádku v cyklu
+                    print(f"Nový rok {rok}")
+                    lines = 1
+                    print(lines)
+                    continue
+            else: #rok == int(row[2]):
+                chyba += 1
+            
+        if rok == int(row[2]): # takže tenhle if vůbec nepotřebuju?
             print(lines)
-            prumer = suma_prutok_rok/lines
+            #prumer = suma_prutok_rok/(lines-chyba)
             #print(prumer)        
         if rok != int(row[2]):                
             if rok == 0:
                 rok = int(row[2])   # nese informaci o aktuálním roku v řádku v cyklu
-                print(f"Nový rok {rok}")
+                print(f"Rok {rok}")
                 print(lines)
             else:
                 print(prumer)
+                prumer = suma_prutok_rok/(lines-chyba)
                 prvni_den[5] = f"   {prumer:.4f}" 
                 writer.writerow(prvni_den)
                 rok = int(row[2])   # nese informaci o aktuálním roku v řádku v cyklu
-                print(f"Nový rok {rok}")
+                print(f"Rok {rok}")
                 lines = 1
                 print(lines)
                 suma_prutok_rok = float(row[5])
+                
         if lines == 1:
             prvni_den = row
         lines += 1
+    prumer = suma_prutok_rok/(lines-chyba)
     print(prumer)
     prvni_den[5] = f"   {prumer:.4f}" 
     writer.writerow(prvni_den)
